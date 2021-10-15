9fe54a1fad0cc4630620568a36bc731183c21947
Nick <nsagias@gmail.com>
2021-10-15 02:14:11 -0400

    Add get all properies code for psgql

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,6 +1,15 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
 /// Users
 
 /**
@@ -72,8 +81,22 @@ const getAllProperties = function(options, limit = 10) {
     limitedProperties[i] = properties[i];
   }
   return Promise.resolve(limitedProperties);
+
 }
+
+const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => {
+      console.log(result.rows);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
 exports.getAllProperties = getAllProperties;
+
 
 
 /**
