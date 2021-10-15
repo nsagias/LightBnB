d58e6116a87ea2c531db8eecc2cff7a08eebbafc
Nick <nsagias@gmail.com>
2021-10-14 21:25:26 -0400

    Add first seed trial

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,36 @@
+
+INSERT INTO
+  users (name, email, password)
+VALUES
+  ('red', 'red@red.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+  ('blue','blue@blue.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+  ('green','green@green.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO 
+  properties (owner_id, title, description, thumbnail_photo_url,  cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+VALUES      
+  (1, 'Red title Home', 'description', 'bnb_thumbs.com/red', 'bnb_cover.com/red', 100, 1, 1, 1, 'Canada', '1 Red Street', 'Red City', 'Ontario', 'M0M0M0', TRUE),
+  (2, 'Blue title Home', 'description', 'bnb_thumbs.com/blue', 'bnb_cover.com/blue', 200, 2, 2, 2, 'Canada', '2 Blue Street', 'Blue City', 'Ontario', 'M0M0M0', TRUE),
+  (3, 'Green title Home', 'description', 'bnb_thumbs.com/green', 'bnb_cover.com/green', 300, 3, 3, 3, 'Canada', '3 Green Street', 'Green City', 'Ontario', 'M0M0M0', TRUE);
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+INSERT INTO 
+  reservations (start_date, end_date, property_id, guest_id)
+VALUES
+  (2021-10-14 00:35:39.70551, 2021-10-15 00:35:39.70551, 1, 1),
+  (2021-10-14 00:35:39.70551, 2021-10-15 00:35:39.70551, 2, 2),
+  (2021-10-14 00:35:39.70551, 2021-10-15 00:35:39.70551, 3, 3);
+
+INSERT INTO
+  property_reviews (guest_id, property_id, reservation_id, rating, message)
+VALUES
+  (1, 1, 1, 5, 'message'),
+  (2, 2, 3, 5, 'message'),
+  (3, 3, 3, 5, 'message');
