accb20d929975a05b0a7b7b42103b53100bf4b21
Nick <nsagias@gmail.com>
2021-10-14 23:55:25 -0400

    clean notes

--- 1_queries/3_property_listings_by_city.sql
+++ 1_queries/3_property_listings_by_city.sql
@@ -1,15 +1,6 @@
 -- \i 1_queries/3_property_listings_by_city.sql
 
 
-
--- SELECT *, AVG(rating) AS average_rating, property_id
--- FROM properties, property_reviews
--- WHERE city = 'Vancouver'
--- GROUP BY properties.id, property_reviews.id
--- ORDER BY cost_per_night
--- LIMIT 10
--- ;
-
 SELECT properties.*, avg(property_reviews.rating) as average_rating
 FROM properties
 JOIN property_reviews ON properties.id = property_id
