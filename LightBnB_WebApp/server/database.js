cd537a73340fe5a91ca4a6fda40d03ba2e2d4cbb
Nick <nsagias@gmail.com>
2021-11-16 13:37:53 -0500

    Write conditional queries based on user input and to match case

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -174,33 +174,60 @@ const getAllProperties = function (options, limit = 10) {
     SELECT properties.*, avg(property_reviews.rating) as average_rating
     FROM properties
     JOIN property_reviews ON properties.id = property_id
+    WHERE true
     `;
 
   // 3
+
   if (options.city) {
     queryParams.push(`%${options.city}%`);
-    queryString += `WHERE city LIKE $${queryParams.length} `;
+    queryString += `AND city ILIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += `AND owner_id = $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night}`);
+    queryString += `AND cost_per_night > $${queryParams.length} `;
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night}`);
+    queryString += `AND cost_per_night < $${queryParams.length} `;
   }
 
   // 4
+ 
+  queryString += `GROUP BY properties.id `;
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `HAVING avg(rating) > $${queryParams.length} `;
+  }
+
+
   queryParams.push(limit);
   queryString += `
-    GROUP BY properties.id
-    ORDER BY cost_per_night
-    LIMIT $${queryParams.length};
-    `;
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
 
   // 5
   console.log(queryString, queryParams);
 
 //   // 6
-  return pool.query(queryString, queryParams).then((res) => res.rows).catch(err => err);
-  // return pool
-  //     .query(queryString, queryParams)
-  //     .then((res) => {
-  //       return res.rows;
-  //     })
-  //     .catch(err => err);
+  // return pool.query(queryString, queryParams).then((res) => res.rows).catch(err => console.log(err));
+  return pool
+      .query(queryString, queryParams)
+      .then((res) => {
+        console.log('hereerererere',res.rows)
+        return res.rows;
+      })
+      .catch(err => console.log('this is err',err));
 };
 
 exports.getAllProperties = getAllProperties;
