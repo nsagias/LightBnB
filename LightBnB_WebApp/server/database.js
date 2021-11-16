c227c27ad7ce5cde6b6bba45f64d04220e0096ba
Nick <nsagias@gmail.com>
2021-11-15 23:24:39 -0500

    add paramaterization

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -131,11 +131,11 @@ const getAllReservations = function(guest_id, limit = 10) {
     FROM reservations
     JOIN properties ON reservations.property_id = properties.id
     JOIN property_reviews ON properties.id = property_reviews.property_id
-    WHERE reservations.guest_id = 1
+    WHERE reservations.guest_id = $1
     AND reservations.end_date < now()::date
     GROUP BY properties.id, reservations.id
     ORDER BY reservations.start_date
-    LIMIT 10;
+    LIMIT $2;
     `;
 
   return pool
