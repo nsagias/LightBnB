4d78b743a63a1f24e7f53f14a5a425e83ce13e05
Nick <nsagias@gmail.com>
2021-11-15 23:36:39 -0500

    Add boilerplate from compass

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -166,13 +166,35 @@ const getAllProperties = function(options, limit = 10) {
 }
 
 
-// const getAllProperties = (options, limit = 10) => {
-//   return pool
-//     .query(`SELECT * FROM properties LIMIT $1`, [limit])
-//     .then((result) => { result.rows})
-//     .catch((err) => {
-//       console.log(err.message);
-//     });
+// const getAllProperties = function (options, limit = 10) {
+//   // 1
+//   const queryParams = [];
+//   // 2
+//   let queryString = `
+//   SELECT properties.*, avg(property_reviews.rating) as average_rating
+//   FROM properties
+//   JOIN property_reviews ON properties.id = property_id
+//   `;
+
+//   // 3
+//   if (options.city) {
+//     queryParams.push(`%${options.city}%`);
+//     queryString += `WHERE city LIKE $${queryParams.length} `;
+//   }
+
+//   // 4
+//   queryParams.push(limit);
+//   queryString += `
+//   GROUP BY properties.id
+//   ORDER BY cost_per_night
+//   LIMIT $${queryParams.length};
+//   `;
+
+//   // 5
+//   console.log(queryString, queryParams);
+
+//   // 6
+//   return pool.query(queryString, queryParams).then((res) => res.rows);
 // };
 
 exports.getAllProperties = getAllProperties;
