19a1d1fd6599d2edb9ef12414a61abb188c146dc
Nick <nsagias@gmail.com>
2021-11-15 23:23:50 -0500

    Add query params

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -125,21 +125,18 @@ exports.addUser = addUser;
 // }
 
 const getAllReservations = function(guest_id, limit = 10) {
-  const queryParams = [email];
+  const queryParams = [guest_id, limit ];
   const queryString = `
-    SELECT *
-    FROM users
-    WHERE email = $1
+    SELECT properties.*, reservations.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = 1
+    AND reservations.end_date < now()::date
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT 10;
     `;
-    // SELECT properties.*, reservations.*, avg(rating) as average_rating
-    // FROM reservations
-    // JOIN properties ON reservations.property_id = properties.id
-    // JOIN property_reviews ON properties.id = property_reviews.property_id
-    // WHERE reservations.guest_id = 1
-    // AND reservations.end_date < now()::date
-    // GROUP BY properties.id, reservations.id
-    // ORDER BY reservations.start_date
-    // LIMIT 10;
 
   return pool
     .query(queryString, queryParams)
